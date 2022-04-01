b3559d7c0df198852469cc13a0612f573e5c9bbd
alex rigney <arign020@gmail.com>
2022-04-01 01:32:48 -0400

    added logic for randomButton to MainActivity.java

--- EECS1022_Project/app/src/main/java/com/example/eecs1022_project/MainActivity.java
+++ EECS1022_Project/app/src/main/java/com/example/eecs1022_project/MainActivity.java
@@ -10,6 +10,7 @@ import android.widget.EditText;
 import org.json.JSONException;
 
 import java.io.IOException;
+import java.util.ArrayList;
 
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
@@ -26,9 +27,35 @@ public class MainActivity extends AppCompatActivity {
     public void submitAnimal(View view) {
         Intent intent = new Intent(this, DisplayAnimal.class);
         EditText editText = (EditText) findViewById(R.id.input);
+        // Get message from EditText box and send to next activity
         String message = editText.getText().toString();
         intent.putExtra(EXTRA_MESSAGE, message);
         startActivity(intent);
+    }
+
+    public void submitRandom(View view) {
+        Intent intent = new Intent(this, DisplayAnimal.class);
+        EditText editText = (EditText) findViewById(R.id.input);
+        // Random button randomly assigns an animal
+        int random = (int) Math.floor(Math.random()*(9 - 0 + 1) + 0);
+//        intent.putExtra(EXTRA_MESSAGE, random);
+//        startActivity(intent);
+        System.out.println("Random number: "+ random);
+
+        // Logic to go in displayRandomAnimal activity
+        ArrayList allAnimals = new ArrayList<String>();
+        allAnimals.add("dog");
+        allAnimals.add("cat");
+        allAnimals.add("mouse");
+        allAnimals.add("cow");
+        allAnimals.add("duck");
+        allAnimals.add("tiger");
+        allAnimals.add("monkey");
+        allAnimals.add("kimodo dragon");
+        allAnimals.add("penguin");
+        allAnimals.add("fish");
+
+        System.out.println(allAnimals.get(random));
     }
 
 }
